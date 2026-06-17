d1980c86e1a54922064ec8e1be3a7cc2552b287e
f-99 <faniomears@gmail.com>
2026-06-17 12:25:12 +0100

    Done: clearDisplay is working

--- js/index.js
+++ js/index.js
@@ -2,13 +2,34 @@ const   calcDisplay = document.querySelector(".display");
 const   container = document.querySelector(".container");
 const   calcButtons = [...document.querySelectorAll(".btn")];
 let     result = document.querySelector(".result");
-
+let     n1 = "";
+let     n2 = "";
+let     total = "";
 
 for (const button of calcButtons )
 {
     button.addEventListener("click", ()=>{
-        calcDisplay.textContent += button.textContent;
+        if ( isOperator( button.textContent) )
+            alert("Operator: " + button.textContent);
+        if (button.textContent.toLocaleLowerCase() === 'c')
+        {
+            clearDisplay();
+        }
+        if (button.textContent.toLocaleLowerCase() !== 'c')
+            calcDisplay.textContent += button.textContent;
     });
+}
+
+function    clearDisplay()
+{
+    let     old = calcDisplay.textContent;
+    let     newText = old.slice(0, -1);
+    calcDisplay.textContent = newText;
+}
+
+function    isOperator( char )
+{
+    return (char === "+" || char === "-" || char === "/" || char === "*");
 }
 
 function    sum( number1, number2 )
